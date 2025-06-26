6578afd971a7317c89e6a85ca7dd0dd659059962
njugus <kayvohnjuguna@gmail.com>
2025-06-26 10:08:12 -0700

    adds a few changes and sets up the server file

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,17 @@
+//import the necessary dependencies
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
+// import fileUpload from 'express-fileUpload'
+import { dirname } from 'path';
+import fileUpload from 'express-fileupload';
+
+
+//gets the pathname of the current file e.g index.js
+const __filename = fileURLToPath(import.meta.url);
+//gets the pathname of the current directory
+const __dirname = dirname(__filename);
+
 
 //create an app
 const app = express()
